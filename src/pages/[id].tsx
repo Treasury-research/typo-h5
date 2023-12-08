51bfe8e647d25df7e3f61fbf42e90a9521138b12
Murraya-paniculata <1395039366@qq.com>
2023-12-08 15:37:19 +0800

    update: id page

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,4 +1,4 @@
-import { Container, HStack } from "@chakra-ui/react";
+import { Box, Container, Image, VStack } from "@chakra-ui/react";
 import { NextSeo } from "components";
 
 export default function Home() {
@@ -12,28 +12,36 @@ export default function Home() {
         overflow="hidden"
         className="no-scrollbar"
       >
-        <HStack
-          justifyContent="center"
-          h="full"
-          background="url('./images/jiguang.png') no-repeat center fixed"
-          backgroundSize="cover"
-        >
-          <div className="w-10/12 blur-[10px] rounded-l bg-[rgba(255,255,255,0.3)] p-7 font-medium">
-            <p className="text-center text-7xl">💻</p>
-            <br />
-            <p className="text-center">
-              We encourage all users to take advantage of the benefits of
-              accessing the web on a computer rather than a mobile device
-            </p>
-            <br />
-            <p>
-              by accessing the web through a desktop or laptop computer, you
-              will have a more seamless and efficient experience
-            </p>
-            <br />
-            <p className="text-right font-medium">KNN3 Team</p>
-          </div>
-        </HStack>
+        <Box h="full">
+          <Image
+            className="absolute top-[50px] left-1/2"
+            style={{ transform: "translateX(-50%)" }}
+            src="./images/logo.svg"
+            alt=""
+          />
+          <VStack
+            justifyContent="center"
+            h="full"
+            background="url('./images/jiguang.png') no-repeat center fixed"
+            backgroundSize="cover"
+          >
+            <div className="w-10/12 blur-[10px] rounded-l bg-[rgba(255,255,255,0.3)] p-7 font-medium">
+              <p className="text-center text-7xl">💻</p>
+              <br />
+              <p>
+                We encourage all users to take advantage of the benefits of
+                accessing the web on a computer rather than a mobile device
+              </p>
+              <br />
+              <p>
+                by accessing the web through a desktop or laptop computer, you
+                will have a more seamless and efficient experience
+              </p>
+              <br />
+              <p className="text-right font-medium">KNN3 Team</p>
+            </div>
+          </VStack>
+        </Box>
       </Container>
     </>
   );
